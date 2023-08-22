9d09274f841885c87d61717d33b8a0ae9501f00b
Gary Ditsch <garyditsch@gmail.com>
2023-08-22 17:44:42 -0400

    moved data source over to airtable, added scatter plot for swims and cleaned up previous code

--- strava_runs.py
+++ strava_runs.py
@@ -1,56 +1,55 @@
 import streamlit as st
 import pandas as pd
+from pyairtable import Api
 import numpy as np
+import plotly.express as px
 import matplotlib.pyplot as plt
 import july
 from july.utils import date_range
 from datetime import date, timedelta
 
+api = Api(st.secrets["AIRTABLE_API_KEY"])
+table = api.table('appAsWd4GyI8WrvPk', 'tblENY9sFTGYzvc0u')
+activities = table.all()
+strava_data = [item['fields'] for item in activities]
+df = pd.DataFrame(strava_data)
+df['Activity Date'] = pd.to_datetime(df['Activity Date'])
+df_2023 = df[(df['Activity Date'] > "2022-12-31")]
+df_2023_swims = df_2023[(df_2023['Activity Type'] == 'Swim')]
+df_sorted = df_2023_swims.sort_values(by='Activity Date', ascending=False)
+df_sorted['Elapsed Time'] = df_sorted['Elapsed Time'].div(60).round(2)
+df_sorted_minutes = df_sorted[['Elapsed Time', 'Day of Year', 'Activity Date']]
+
 st.title('2023 Swim Workouts')
 
 DATE_COLUMN = 'Activity Date'
-# DATA_URL = 'https://gist.githubusercontent.com/garyditsch/b2cc06fdbc2bb978639af05a1c8ac71e/raw/470416ff53d0062e2c0ac5aa71b354f86ba3e813/2023_Swims.csv'
-DATA_URL = 'https://gist.githubusercontent.com/garyditsch/b2cc06fdbc2bb978639af05a1c8ac71e/raw/0869da48787e2440b084ff00758a7a915e79a3f6/2023_Swims.csv'
 DATES = date_range("2023-01-01", "2023-12-31")
 
 @st.cache_data
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    # lowercase = lambda x: str(x).lower()
-    # data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    data = df_sorted[0:nrows]
     return data
 
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(52)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache_data)")
-
-print(data.head())
+data = load_data(60)
+data_load_state.text("Data has loaded.")
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
+fig = px.scatter(
+    df_sorted,
+    x="Activity Date",
+    y="Elapsed Time",
+    size_max=60,
+    labels={
+        "Elapsed Time": "Minutes" 
+    }
+)
 
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-st.title('Workouts grouped by time of day')
-st.bar_chart(hist_values) 
-
-# print(data[DATE_COLUMN])
-
-
-# hour_to_filter = st.slider('hour', 0, 23, 17)
-# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-# st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-# st.map(filtered_data)
-
-# datetest = date_range("2020-01-01", "2020-12-31")
-# data2 = np.random.randint(0, 14, len(datetest))
-# print(len(data2))
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 first_date = date(2023, 1, 1)
 duration = timedelta(days=365)
@@ -64,9 +63,6 @@ for d in range(duration.days + 1):
         swim_year.append(10)
     else: 
         swim_year.append(5)
-
-print(swim_year)
-print(len(swim_year))
 
 fig, ax = plt.subplots()
 
